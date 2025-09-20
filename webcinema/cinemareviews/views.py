097bd6def6494cab9a033d2f92fccca5fe3d8360
student-pro <student-pro@b301-2.khpk.ru>
2025-09-20 11:05:52 +0700

    Started index page

--- webcinema/cinemareviews/views.py
+++ webcinema/cinemareviews/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Movie, Genre, Reviewer, Review
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all().prefetch_related('genres')
+    return render(request, 'index.html', {'movies': movies})
