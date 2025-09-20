6b40388dd15db12c9834cf8d79115f67a7af5f13
student-pro <student-pro@b301-2.khpk.ru>
2025-09-20 09:48:59 +0700

    Created models

--- webcinema/cinemareviews/models.py
+++ webcinema/cinemareviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=70, unique=True)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+class Reviewer(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, related_name="reviews", on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(Reviewer, related_name="reviews", on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    text = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=timezone.now())
